acad7554b2c82b7c17e6b9821d1d539068f249fd
jane20030605 <137973965+jane20030605@users.noreply.github.com>
2024-04-12 16:01:28 +0800

    20240412-連線測試

--- app/src/main/java/com/example/myapplication/ui/Login/LoginFragment.java
+++ app/src/main/java/com/example/myapplication/ui/Login/LoginFragment.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
