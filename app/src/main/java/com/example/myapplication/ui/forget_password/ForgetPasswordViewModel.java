74364bb7e648b3d339aecef09875f7efbddd27fa
jane20030605 <137973965+jane20030605@users.noreply.github.com>
2024-05-08 17:38:39 +0800

    20240508-資料更新

---
+++ app/src/main/java/com/example/myapplication/ui/forget_password/ForgetPasswordViewModel.java
@@ -0,0 +1,7 @@
+package com.example.myapplication.ui.forget_password;
+
+import androidx.lifecycle.ViewModel;
+
+public class ForgetPasswordViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/myapplication/ui/forget_password/forget_passwordFragment.java
@@ -0,0 +1,38 @@
+package com.example.myapplication.ui.forget_password;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapplication.R;
+
+public class forget_passwordFragment extends Fragment {
+
+    private ForgetPasswordViewModel mViewModel;
+
+    public static forget_passwordFragment newInstance() {
+        return new forget_passwordFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_forget_password, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ForgetPasswordViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
